5e7f00049d2bbfbef4fa4de9c04699e4281147b0
Administrator <Administrator@192.168.9.163>
2017-05-12 14:11:31 +0800

    增加login

---
+++ src/main/java/com/chuangge/user/login/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.chuangge.user.login.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	/**
+	 * 验证登陆
+	 * @return
+	 */
+	@RequestMapping(value = "outside/user/login")
+	@ResponseBody
+	public Map login(String username, String password, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		Map returnMap = new HashMap();
+		returnMap.put("returnCode", 1);
+		returnMap.put("returnMessage", "success");
+
+		return returnMap;
+	}
+}
