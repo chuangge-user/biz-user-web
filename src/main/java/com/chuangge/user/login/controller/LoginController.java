2f54a9c383673aa42a244da4329caa2e30ef2ff2
Administrator <Administrator@192.168.9.178>
2017-05-15 18:09:14 +0800

    增加发送短信

--- src/main/java/com/chuangge/user/login/controller/LoginController.java
+++ src/main/java/com/chuangge/user/login/controller/LoginController.java
@@ -4,17 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.chuangge.redis.util.RedisUtils;
 import com.chuangge.user.common.util.Jsonp;
 import com.chuangge.user.login.vo.LoginRequestVo;
+import com.chuangge.user.service.SmsSendService;
+import com.chuangge.user.util.AppConfigConstants;
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private SmsSendService smsSendService;
 	
 	
 	/*****
@@ -51,6 +59,10 @@ public class LoginController {
 		int a = (int)(Math.random()*(9999-1000+1))+1000;
 		RedisUtils.opsForValue().set("getVerifyCode"+mobile,a+"",5,TimeUnit.MINUTES);
 		System.out.println(RedisUtils.opsForValue().get("getVerifyCode"+mobile));
+		
+		JSONObject param = new JSONObject();
+		param.put("code", a+"");
+		System.out.println(smsSendService.sendSms("15201590885", param.toJSONString()));
 		return jsonP;
 	}
 	
